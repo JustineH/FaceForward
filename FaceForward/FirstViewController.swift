065a383dbc75817c3557e175a012a5a56dd35d5a
carmen-c <carmen.csmwc@gmail.com>
2016-12-14 14:33:37 -0800

    Initial Commit

---
+++ FaceForward/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FaceForward
+//
+//  Created by carmen cheng on 2016-12-14.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
