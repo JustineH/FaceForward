d004bffa48d7688df54575a09f37982b78355fcf
carmen-c <carmen.csmwc@gmail.com>
2016-12-15 19:17:46 -0800

    out dates hidden

--- FaceForward/CalendarViewController.swift
+++ FaceForward/CalendarViewController.swift
@@ -83,27 +83,30 @@ extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendar
         formatter.dateFormat = "MM dd yyyy"
         
         let startDate = formatter.date(from: "12 09 2016")!
-        let endDate = Date()
+        let endDate = formatter.date(from: "12 09 2100")!
         let parameters = ConfigurationParameters(startDate: startDate,
                                                  endDate: endDate,
                                             numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
                                                 calendar: Calendar.current,
                                          generateInDates: .forAllMonths,
-                                        generateOutDates: .tillEndOfGrid,
+                                        generateOutDates: .off,
                                           firstDayOfWeek: .sunday)
         return parameters
     }
     
     func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
-        let myCustomCell = cell as! CellView
         
+        let myCustomCell = cell as! CellView
         myCustomCell.dayLabel.text = cellState.text
         
         if cellState.dateBelongsTo == .thisMonth {
-            myCustomCell.dayLabel.textColor = UIColor.black
+            myCustomCell.isHidden = false
         } else {
-            myCustomCell.dayLabel.textColor = UIColor.lightGray
+            myCustomCell.isHidden = true
         }
+        
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelection(view: cell, cellState: cellState)
     }
     
 }
