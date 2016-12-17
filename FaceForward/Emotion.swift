f994ef7625ca946799c70bdd82f5896ad0f55b31
carmen-c <carmen.csmwc@gmail.com>
2016-12-16 17:16:03 -0800

    added to realm object

--- FaceForward/Emotion.swift
+++ FaceForward/Emotion.swift
@@ -7,9 +7,43 @@
 //
 
 import UIKit
-import Realm
+import RealmSwift
 
-class Emotion: RLMObject {
-
+// create a list of stuff after the photo view controller and init this object
+class Emotion: Object {
+    dynamic var moodInput = ""
+    dynamic var sleepInput = 0.0
+    let exerciseInput: Bool? = nil
+    dynamic var peopleInput = ""
+    dynamic var notesInput = ""
+    dynamic var date: Date?
     
+    let emotionDictionary = Dictionary<String, Any>() //dictionary of string:anyObjects
+    let emotionArray = Array<Any>() //array of anyObjects
+    
+    override var description: String { return "Emotion{\(moodInput), \(sleepInput), \(exerciseInput), \(peopleInput), \(notesInput), \(date), \(emotionDictionary), \(emotionArray)}"}
 }
+
+let realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "Emotion"))
+
+
+// TO CREATE A NEW OBJECT
+//let dog = Emotion()
+//dog.moodInput =
+//    dog.sleepInput =
+//    dog.exerciseInput =
+//    dog.peopleInput =
+//    dog.notesInput =
+//    dog.emotionArray =
+//    dog.emotionDictionary =
+
+
+// TO SAVE TO REALM?
+//let realm = try! Realm()
+//try! realm.write {
+//    realm.add(dog)
+//}
+
+//TO RETRIEVE FROM REALM?
+//let realm = try! Realm()
+//let results = realm.objects(dog)
