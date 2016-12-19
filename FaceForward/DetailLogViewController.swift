cb36e7081586e2a3cd9ab34337d7f6c49a24e204
carmen-c <carmen.csmwc@gmail.com>
2016-12-19 11:01:01 -0800

    setup detailLog

--- FaceForward/DetailLogViewController.swift
+++ FaceForward/DetailLogViewController.swift
@@ -7,29 +7,79 @@
 //
 
 import UIKit
+import Charts
+import RealmSwift
 
 class DetailLogViewController: UIViewController {
-
+    
+//MARK: Properties
+    @IBOutlet weak var titleDateLabel: UILabel!
+    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var sleepLabel: UILabel!
+    @IBOutlet weak var peopleLabel: UILabel!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var chartView: LineChartView!
+    
+    weak var axisFormatDelegate: IAxisValueFormatter?
+    var passedSurveyDetails: Survey
+    
+//MARK: View
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        axisFormatDelegate = self
+        updateChartWithData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//MARK: Labels
+    func updateLabels(){
+        titleDateLabel.text = "On \(date) you were..."
+        moodLabel.text = passedSurveyDetails.moodInput!
+        exerciseLabel.text = passedSurveyDetails.exerciseInput!
+        sleepLabel.text = passedSurveyDetails.sleepInput!
+        peopleLabel.text = passedSurveyDetails.peopleInput ?? ""
+        notesTextView.text = passedSurveyDetails.notesInput ?? ""
     }
-    */
+    
+    
+//MARK: Chart
+    func updateChartWithData() {
+        var emotionEntries: [Survey] = []
+        let savedEmotions = getEmotionsFromRealm()
+        
+        for i in 0..<savedEmotions.count {
+            let timeIntervalForDate: TimeInterval = Survey.date.timeIntervalSince1970
+            let emotionEntry = LineChartDataEntry(x:Double(i), y:Double(savedEmotions[i].count))
+            emotionEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = LineChartDataSet(values: emotionEntries, label: "emotions")
+        let chartData = LineChartData(dataSet: chartDataSet)
+        chartView.data = chartData
+        
+        let xaxis = chartView.xAxis
+        xaxis.valueFormatter = axisFormatDelegate
+    }
 
+
+    func getEmotionsFromRealm() -> Results<Survey>{
+        do{
+            let realm = try Realm()
+            return realm.objects(Survey)
+        } catch let error as NSError {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+
+}
+
+
+//MARK: IAxisValueFormatter
+extension DetailLogViewController: IAxisValueFormatter {
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm.ss"
+        return dateFormatter.string(from: Date(timeIntervalSince1970:value))
+    }
 }
