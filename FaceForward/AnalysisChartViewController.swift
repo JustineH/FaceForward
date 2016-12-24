028d7284e9b4a333e5ed9641a2a38a7ac59309b7
Justine <justine.m.h@gmail.com>
2016-12-23 17:35:35 -0800

    Charts cocoapod not working out

--- FaceForward/AnalysisChartViewController.swift
+++ FaceForward/AnalysisChartViewController.swift
@@ -15,8 +15,9 @@ class AnalysisChartViewController: UIViewController, ChartViewDelegate {
     
     @IBOutlet weak var pieChartView: PieChartView!
     
-    dynamic var emotionsArray: [String]!
-    var dict: Emotion = [String:Double]()
+    dynamic var emotionsToPassToPieChart = [String:Double]()
+    dynamic var emotionsArray = [String]()
+    var dict = Emotion()
     
     
     @IBAction func nextToSuggestionsButton(_ sender: Any) {
@@ -28,8 +29,18 @@ class AnalysisChartViewController: UIViewController, ChartViewDelegate {
         pieChartView.delegate = self
         pieChartView.notifyDataSetChanged()
         
-        let emotionsArray = ["Anger", "Contempt", "Disgust", "Fear", "Sadness", "Surprise", "Happiness", "Neutral"]
-        let emotionsPercentage = [0.0]
+        emotionsToPassToPieChart["Anger"] = dict.anger
+        emotionsToPassToPieChart["Contempt"] = dict.contempt
+        emotionsToPassToPieChart["Disgust"] = dict.disgust
+        emotionsToPassToPieChart["Fear"] = dict.fear
+        emotionsToPassToPieChart["Sadness"] = dict.sadness
+        emotionsToPassToPieChart["Surprise"] = dict.surprise
+        emotionsToPassToPieChart["Happiness"] = dict.happiness
+        emotionsToPassToPieChart["Neutral"] = dict.neutral
+
+    //    emotionsArray = emotionsToPassToPieChart.keys
+        
+        let emotionsPercentage = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         
         setChart(dataPoints: emotionsArray, values: emotionsPercentage)
     }
@@ -40,6 +51,7 @@ class AnalysisChartViewController: UIViewController, ChartViewDelegate {
         legend.enabled = true
         legend.horizontalAlignment = .left
         legend.drawInside = false
+        legend.orientation = .horizontal
         pieChartView.chartDescription?.enabled = false
         pieChartView.drawHoleEnabled = false
         pieChartView.legend.enabled = true
@@ -48,47 +60,28 @@ class AnalysisChartViewController: UIViewController, ChartViewDelegate {
         
         var dataEntries: [PieChartDataEntry] = []
         
-        var emotionsToPassToPieChart = [String:Double]()
-        emotionsToPassToPieChart["Anger"] = dict.anger
-        emotionsToPassToPieChart["Contempt"] = dict.contempt
-        emotionsToPassToPieChart["Disgust"] = dict.disgust
-        emotionsToPassToPieChart["Fear"] = dict.fear
-        emotionsToPassToPieChart["Sadness"] = dict.sadness
-        emotionsToPassToPieChart["Surprise"] = dict.surprise
-        emotionsToPassToPieChart["Happiness"] = dict.happiness
-        emotionsToPassToPieChart["Neutral"] = dict.neutral
        
         for emotion in emotionsToPassToPieChart.keys {
  
-            let dataEntry = PieChartDataEntry(value: emotionsToPassToPieChart[emotion], label: emotion)
+            let dataEntry = PieChartDataEntry(value: emotionsToPassToPieChart[emotion]!, label: emotion)
           //  let dataEntry = PieChartDataEntry(value: percentage, label: emotion)
            // let emotion = formatter.stringFromArray(emotion)
             
             
             dataEntries.append(dataEntry)
         }
+        
         let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "")
-        var iPieChartDataSet: [IChartDataSet] = []
-        iPieChartDataSet.append(pieChartDataSet)
-    
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
         
-        let pieChartData = PieChartData(dataSets: iPieChartDataSet)
         pieChartView.data = pieChartData
-        
-     
-      //  let pieChartData = PieChartData(dataSet: pieChartDataSet)
-      //  pieChartView.data = pieChartData
-      //  pieChartView.animate(xAxisDuration: 1.0)
-       
-     //   let chartFormatter = ChartFormatter()
-     //   pieChart.xAxis.valueFormatter = xAxis.valueFormatter
+        pieChartView.notifyDataSetChanged()
+        pieChartView.animate(xAxisDuration: 2.0)
         
       //  pieChartView.setNeedsDisplay()
         
     }
     
-  
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
