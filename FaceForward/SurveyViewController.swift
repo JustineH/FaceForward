5064ed3d6b2db1144f0f3281e6bc899ffb19036c
carmen-c <carmen.csmwc@gmail.com>
2017-01-12 14:27:02 -0800

    pause music when in survey

--- FaceForward/SurveyViewController.swift
+++ FaceForward/SurveyViewController.swift
@@ -105,6 +105,7 @@ class SurveyViewController: UIViewController, UITextFieldDelegate {
 //        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
 //        imageView.contentMode = .scaleAspectFit
 //        imageView.clipsToBounds = true
+        RadioPlayer.sharedInstance.pause()
         Styling.styleButton(button: nextButton)
         self.peopleTextField.delegate = self;
         peopleTextField.textColor = Styling.Colors.fontBody
