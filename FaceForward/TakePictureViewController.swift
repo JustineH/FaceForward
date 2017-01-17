7a8aecce9741394062e904b7e5462c977d4b6b84
Justine <justine.m.h@gmail.com>
2017-01-17 03:18:08 -0800

    Adjust Suggestions Storyboard

--- FaceForward/TakePictureViewController.swift
+++ FaceForward/TakePictureViewController.swift
@@ -38,7 +38,7 @@ class TakePictureViewController: UIViewController, UIImagePickerControllerDelega
     /// Brings up camera to take a photo
     @IBAction func loadImageButtonTapped(_ sender: UIButton) {
         imagePicker.allowsEditing = false
-        imagePicker.sourceType = .photoLibrary
+        imagePicker.sourceType = .camera
         
         present(imagePicker, animated: true, completion: nil)
     }
