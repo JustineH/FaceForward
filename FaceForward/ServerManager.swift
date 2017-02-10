4fd39fb6e4d7731dcdc0377289c7cfd0781879f2
Justine <justine.m.h@gmail.com>
2017-02-09 20:55:43 -0800

    Update ServerManager to handle images that are too large/small and add screenshots of app for README file

--- FaceForward/ServerManager.swift
+++ FaceForward/ServerManager.swift
@@ -13,28 +13,37 @@ import Alamofire
 
 let microsoftUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize"
 let microsoftKey = FFConstants.microsoftKey
+fileprivate let fourMB = 4000000
 
 class ServerManager: NSObject {
     
     class func emotions(from image: UIImage, completion:@escaping ([String:Double])->()) {
 
-        let imageData = UIImageJPEGRepresentation(image, 1.0)!
+        let data = imageData(image, belowSize: fourMB)
         let headers = [
             "Ocp-Apim-Subscription-Key": microsoftKey,
             "Content-Type": "application/octet-stream"
         ]
-        Alamofire.upload(imageData, to: microsoftUrl, headers: headers)
+        Alamofire.upload(data, to: microsoftUrl, headers: headers)
         .responseJSON
         {response in
             switch response.result {
                 case .success:
                     let json = JSON(response.result.value!)
+                    
+                    if let error = json["error"].dictionary {
+                        // Gets called when an image is too big, too small or for other reasons
+                        let errorMessage = error["message"]?.string
+                        print("error \(errorMessage)")
+                    }
+                    
                     var result = [String:Double]()
                     if let dictionary = json[0]["scores"].dictionary {
                         for (name, value) in dictionary {
                             result[name] = value.double
                         }
                     }
+                    
                     completion(result)
                 
                 case .failure(let error):
@@ -42,6 +51,28 @@ class ServerManager: NSObject {
                     completion(["\(error.localizedDescription)":0])
             }
         }
+    }
+    
+    /**
+     Create data from a UIImage
+     
+     - parameter image: The image to create the data from
+     - parameter size: The size, in bytes, that the data has to be below
+     - returns: data
+     
+    */
+    private class func imageData(_ image: UIImage, belowSize limit: Int) -> Data {
+        
+        var quality:CGFloat = 1.0
+        
+        var imageData = UIImageJPEGRepresentation(image, quality)!
+       
+        while (imageData.count > limit) {
+            quality -= 0.1
+            imageData = UIImageJPEGRepresentation(image, quality)!
+        }
+        
+        return imageData
     }
 
 }
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ FaceForward is an iOS app that helps users set out each day on their own terms. 
 
 Users can choose to later check in and do another analysis to see how they are feeling, and if there have been any changes in their mood. All of their assessment readings are tracked in a calendar that they can then look back on to reflect on their previous states. While we do not claim that this is scientifically proven, FaceForward gives a non-biased analysis of a user’s current emotional state, allowing them to evaluate how they interact with others, and allow them the opportunity to improve their mood and put their best face forward. 
 
+![FaceFoward Calendar](http://imgur.com/vfRY6tk) ![FaceFoward Calendar](http://imgur.com/SJp79Ic) ![FaceFoward Calendar](http://imgur.com/EabvLbV) ![FaceFoward Calendar](http://imgur.com/TKJezGE) ![FaceFoward Calendar](http://imgur.com/1HSsHJO)
+
 ## Get it on the App Store
 Coming soon to the App Store. Stay tuned!
 <br>
